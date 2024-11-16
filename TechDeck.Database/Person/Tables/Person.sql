a50415a9982ca7ee52117011f727efc536eb2a76
Jamie-Lee Johns <JLJohns@liaisonfs.com>
2024-11-16 13:11:49 +0000

    SQL Table and Schema

---
+++ TechDeck.Database/Person/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Person].[Person] (
+    [Id]           INT             IDENTITY (1, 1) NOT NULL,
+    [Forename]     NVARCHAR (30)   NOT NULL,
+    [Surname]      NVARCHAR (30)   NOT NULL,
+    [Email]        NVARCHAR (320)  NOT NULL,
+    [PasswordHash] VARBINARY (MAX) NOT NULL,
+    CONSTRAINT [PK_User.User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
