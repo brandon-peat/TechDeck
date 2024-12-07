38a0418764235c4a804aacb835d384a9dcd83d1e
Brandon Peat <brandonpeat@hotmail.com>
2024-12-07 10:52:40 +0000

    Merged PR 3: User Story 30 - Welcome Email
    Signing up now sends a welcome email with a link to log in
    
    Related work items: #30

--- TechDeck.Core/People/SignUpCommand.cs
+++ TechDeck.Core/People/SignUpCommand.cs
@@ -1,4 +1,6 @@
 ﻿using MediatR;
+using System.Net;
+using System.Net.Mail;
 using TechDeck.Core.Models;
 
 namespace TechDeck.Core.People
@@ -23,6 +25,16 @@ namespace TechDeck.Core.People
             };
 
             await personRepository.Create(person, cancellationToken);
+
+            var smtpClient = new SmtpClient("smtp.mailersend.net")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("MS_jQQnbD@trial-z86org809wn4ew13.mlsender.net", "5OPlK4awxe6yS4rO"),
+                EnableSsl = true,
+            };
+
+            var body = $"Welcome {request.FirstName} {request.LastName}. Your TechDeck account has been successfully registered. Please click here to log in: http://localhost:4200/log-in";
+            smtpClient.Send("no-reply@trial-z86org809wn4ew13.mlsender.net", request.Email, "Welcome to TechDeck", body);
 
             return ResponseViewModel.Success();
         }
