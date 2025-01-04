2bc5863b23f58c29f9cbeac64989d709a5171d34
Jamie-Lee Johns <jamieleejohns@icloud.com>
2025-01-04 16:38:21 +0000

    reloads sessions

--- TechDeck.Web/src/app/security/security.service.ts
+++ TechDeck.Web/src/app/security/security.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, signal, WritableSignal } from "@angular/core";
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { LocalStorageService } from "./local-storage.service";
 import { UserAuthBase } from "./user-auth-base";
 
 @Injectable({
@@ -10,8 +11,20 @@ export class SecurityService {
   public isLoggedIn: WritableSignal<boolean> = signal(false);
   public user: WritableSignal<UserAuthBase | null> = signal(null);
 
+  constructor(private readonly localStorageService: LocalStorageService) {}
+
+  public tryReloadSession(): void {
+    const auth = this.localStorageService.getData('auth');
+
+    if (auth) {
+      this.userIsLoggedIn(JSON.parse(auth));
+    }
+  }
+
   public userIsLoggedIn(user: UserAuthBase): void {
     this.isLoggedIn.set(true);
+
+    this.localStorageService.saveData('auth', JSON.stringify(user));
 
     const helper = new JwtHelperService();
 
