8688218c4a01756e56ac531521c124a815890e23
Brandon Peat <brandonpeat@hotmail.com>
2025-01-04 17:20:52 +0000

    re-styled new post form and now non-logged-in users are redirected from home to login page

--- TechDeck.Web/src/app/home/home.component.ts
+++ TechDeck.Web/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, Signal } from '@angular/core';
+import { Component, Signal, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { SecurityService } from '../security/security.service';
 import { UserAuthBase } from '../security/user-auth-base';
 import { PostService } from '../services/post.service';
 
+import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 
 @Component({
@@ -12,8 +13,13 @@ import { MessageService } from 'primeng/api';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public isLoggedIn: Signal<boolean>;
+  public user: Signal<UserAuthBase | null>;
+  public showForm: boolean = false;
+
   constructor(
+    private router: Router,
     private securityService: SecurityService,
     private postService: PostService,
     private messageService: MessageService) {
@@ -22,20 +28,28 @@ export class HomeComponent {
     this.user = securityService.user;
   }
 
-  public isLoggedIn: Signal<boolean>;
-  public user: Signal<UserAuthBase | null>;
-  public showForm: boolean = false;
+  public ngOnInit(): void {
+    if (!this.securityService.isLoggedIn()) {
+      this.router.navigateByUrl('/log-in')
+    }
+  }
 
   public newPostForm = new FormGroup({
     text: new FormControl('', [Validators.required, Validators.maxLength(500)]),
   })
+  
+  public showNewPostForm(): void {
+    if(this.showForm == true) this.showForm = false;
+    else {
+      this.showForm = true;
+      this.text.markAsUntouched();
+    }
+  }
+
   get text() {
     return this.newPostForm.controls.text;
   }
-  public showNewPostForm(): void {
-    this.showForm = true;
-  }
-
+  
   onSubmit() {
     this.postService.createPost(this.text.value!).subscribe(() =>
       this.messageService.add({
